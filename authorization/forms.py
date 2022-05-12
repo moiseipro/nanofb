d10b66c57ecfab125759d9eb1547c6649d5de92b
Niayshin <shavnick.main@gmail.com>
2022-05-12 14:10:02 +0300

    users nickname remove, update auth form

--- authorization/forms.py
+++ authorization/forms.py
@@ -1,14 +1,23 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from users.models import User
+from users.models import User, UserPersonal
+
+
+text_widget = forms.TextInput(attrs={
+    'class': 'form-control form-control-sm',
+})
+password_widget = forms.PasswordInput(attrs={
+    'class': 'form-control form-control-sm'
+})
 
 
 class NewUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    
+    email = forms.EmailField(required=True, widget=text_widget, label="Почтовый ящик")
+    password1 = forms.CharField(required=True, widget=password_widget, label="Пароль")
+    password2 = forms.CharField(required=True, widget=password_widget, label="Подтверждение пароля")
     class Meta:
         model = User
-        fields = ("email", "username")
+        fields = ["email"]
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
@@ -18,6 +27,31 @@ class NewUserForm(UserCreationForm):
         return user
 
 
+class NewUserPersonalForm(forms.ModelForm):
+    last_name = forms.CharField(required=True, widget=text_widget, label="Фамилия")
+    first_name = forms.CharField(required=True, widget=text_widget, label="Имя")
+    date_birthsday = forms.DateField(required=False, 
+        input_formats=['%d/%m/%Y'],
+        widget=forms.DateInput(attrs={
+            'class': 'form-control form-control-sm',
+            'type': 'date'
+        }),
+        label="Дата рождения")
+    city = forms.CharField(required=False, widget=text_widget, label="Город")
+    phone = forms.CharField(required=False, widget=text_widget, label="Телефон")
+    class Meta:
+        model = UserPersonal
+        fields = ["last_name", "first_name", "city", "date_birthsday", "phone"]
+    def save(self, user_id=None, commit=True):
+        personal = super(NewUserPersonalForm, self).save(commit=False)
+        personal.user = user_id
+        personal.permissions = {}
+        if commit and user_id:
+            personal.save()
+        return personal
+
+
 class NewLoginForm(AuthenticationForm):
-    username = forms.EmailField(required = True)
+    username = forms.EmailField(required = True, widget=text_widget)
+    password = forms.CharField(required=True, widget=password_widget, label="Пароль")
 
