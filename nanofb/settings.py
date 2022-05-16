85f42685d9dc149b4ee7657666d846dd8eceb29d
moiseipro <sasha.makeeff@gmail.com>
2022-05-16 12:58:37 +0300

    Add host_header for pythonanywhere

--- nanofb/settings.py
+++ nanofb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(8i@$r9s^-es5gveieq%!ao9d&fvq(=*&s^s2w&c$!kgon&hdh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nanofb.pythonanywhere.com']
 
 
 # Application definition
