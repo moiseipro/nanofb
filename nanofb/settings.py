87a1cbd9fcd239b61da17697e64b3c32b94bf81b
moiseipro <sasha.makeeff@gmail.com>
2022-05-24 14:26:50 +0300

    Add django rest framefork

--- nanofb/settings.py
+++ nanofb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'rest_framework',
 
     'nanofootball.apps.NanofootballConfig',
     'users.apps.UsersConfig',
