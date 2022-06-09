8615cf769b3bfff6584ff6db9d3b4d8569089abf
moiseipro <sasha.makeeff@gmail.com>
2022-06-09 17:45:36 +0300

    Translation optimization. Extension of the version and group models.

--- version/admin.py
+++ version/admin.py
@@ -1,5 +1,34 @@
 from django.contrib import admin
-from .models import Section, Version
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.models import Group
+
+from .models import Section, Version, CustomGroup
+from django.utils.translation import gettext_lazy as _
+
 
 # Register your models here.
-admin.site.register([Section, Version])
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "updated_date")
+
+
+class GroupInline(admin.StackedInline):
+    model = CustomGroup
+    can_delete = False
+    verbose_name_plural = _('custom groups')
+
+
+class GroupAdmin(BaseGroupAdmin):
+    inlines = (GroupInline, )
+
+
+# Re-register GroupAdmin
+admin.site.unregister(Group)
+admin.site.register(Group, GroupAdmin)
--- version/models.py
+++ version/models.py
@@ -1,20 +1,41 @@
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Permission, Group
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.utils.translation import pgettext_lazy as _p
 
 
 # Create your models here.
+
+class CustomGroup(models.Model):
+    def __str__(self):
+        return "{}".format(self.group.name)
+
+    group = models.OneToOneField(
+        Group,
+        unique=True,
+        on_delete=models.CASCADE
+    )
+    price = models.DecimalField(
+        verbose_name=_('price'),
+        help_text=_("The cost of the Group in the currency"),
+        max_digits=8,
+        decimal_places=2,
+        default=0.00
+    )
+
+    def __str__(self):
+        return self.group.name
 
 
 class Section(models.Model):
     name = models.CharField(
         max_length=255,
-        verbose_name=_('Name'),
+        verbose_name=_('title'),
         help_text=_("Section name")
     )
     tag = models.CharField(
         max_length=100,
-        verbose_name=_('Tag'),
+        verbose_name=_('tag'),
         help_text=_("Section tag")
     )
     objects = models.Manager()
@@ -22,7 +43,7 @@ class Section(models.Model):
     @classmethod
     def get_default_pk(cls):
         section, created = cls.objects.get_or_create(
-            name='NF', tag='Default')
+            name='NF', tag=_('Default'))
         return section.pk
 
     def __str__(self):
@@ -32,31 +53,41 @@ class Section(models.Model):
 class Version(models.Model):
     name = models.CharField(
         max_length=255,
-        verbose_name=_('Name'),
+        verbose_name=_('title'),
         help_text=_("Version name")
     )
     updated_date = models.DateField(
-        verbose_name=_('Last update'),
+        verbose_name=_('last update'),
         help_text=_("Version update date"),
         auto_now=True
     )
     is_active = models.BooleanField(
-        verbose_name=_('Active'),
+        verbose_name=_('active'),
         help_text=_('The version is active for user selection'),
         default=False
+    )
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_("groups"),
+        blank=True,
+        help_text=_(
+            "The groups this version belongs to. A version will get all permissions "
+            "granted to each of their groups."
+        ),
     )
     permissions = models.ManyToManyField(
         Permission,
         verbose_name=_("permissions"),
         blank=True,
+        help_text=_("Specific permissions for this version."),
     )
     tag = models.CharField(
         max_length=100,
-        verbose_name=_('Tag'),
+        verbose_name=_('tag'),
         help_text=_("Version tag")
     )
     price = models.DecimalField(
-        verbose_name=_('Price'),
+        verbose_name=_('price'),
         help_text=_("The cost of the version in the currency"),
         max_digits=8,
         decimal_places=2,
