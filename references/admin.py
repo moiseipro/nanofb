a8ddaa0b711abcab40bbd923bad5e00527659f24
moiseipro <sasha.makeeff@gmail.com>
2022-06-29 19:28:52 +0300

    Fixed a conflict in reference/admin.py.

--- references/admin.py
+++ references/admin.py
@@ -1,27 +1,13 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from references.models import VideoSource
 from references.models import ExsBall, ExsGoal, ExsWorkoutPart, ExsCognitiveLoad, ExsCategory
 from references.models import ExsPurpose, ExsStressType, ExsCoaching
-=======
-from references.models import VideoSource, ExsBall, UserSeason
->>>>>>> 9ded976b875389ff09a9a86bb3e7d17c615b80d6
+from references.models import VideoSource, UserSeason
 
 # Register your models here.
-admin.site.register([VideoSource, ExsBall])
+admin.site.register([VideoSource])
+admin.site.register([ExsBall, ExsGoal, ExsWorkoutPart, ExsCognitiveLoad, ExsCategory])
+admin.site.register([ExsPurpose, ExsStressType, ExsCoaching])
 
-<<<<<<< HEAD
-admin.site.register(ExsBall)
-admin.site.register(ExsGoal)
-admin.site.register(ExsWorkoutPart)
-admin.site.register(ExsCognitiveLoad)
-admin.site.register(ExsCategory)
-
-admin.site.register(ExsPurpose)
-admin.site.register(ExsStressType)
-admin.site.register(ExsCoaching)
-
-=======
 #For Test
 admin.site.register([UserSeason])
->>>>>>> 9ded976b875389ff09a9a86bb3e7d17c615b80d6
