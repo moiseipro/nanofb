49b9c16a423094d4e60f8c66c208dc55c11c0703
moiseipro <sasha.makeeff@gmail.com>
2022-09-22 15:36:06 +0300

    New migration and collectstatic.

---
+++ static/trainings/js/view_protocol.js
@@ -0,0 +1,38 @@
+$(window).on('load', function (){
+
+
+})
+
+function ajax_protocol_training(method, data, action = '', id = '', func = '') {
+
+    let url = "/trainings/api/protocol/"
+    if(id !== '') url += `${id}/`
+    if(func !== '') url += `${func}/`
+
+    $('.page-loader-wrapper').fadeIn();
+
+    return $.ajax({
+            headers:{"X-CSRFToken": csrftoken },
+            url: url,
+            type: method,
+            dataType: "JSON",
+            data: data,
+            success: function(data){
+                console.log(data)
+                if(data.status == 'exercise_limit'){
+                    swal(gettext('Training '+action), gettext('The limit of exercises for the selected group has been reached'), "error");
+                }if(data.status == 'exercise_got' || data.status == 'sort_exercise'){
+
+                } else {
+                    swal(gettext('Training '+action), gettext('Training action "'+action+'" successfully!'), "success");
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+                console.log(errorThrown)
+                swal(gettext('Training '+action), gettext('Error when action "'+action+'" the training!'), "error");
+            },
+            complete: function () {
+                $('.page-loader-wrapper').fadeOut();
+            }
+        })
+}
