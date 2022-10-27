a09fa756d4d447a30473a1bc94c975fff2601755
moiseipro <sasha.makeeff@gmail.com>
2022-10-27 10:17:34 +0300

    Fix middleware for Team and Season

--- references/middleware/ReferencesMiddleware.py
+++ references/middleware/ReferencesMiddleware.py
@@ -9,12 +9,13 @@ class TeamAndSeasons:
     def __call__(self, request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
-        if request.user.club_id is not None:
-            request.teams_list = ClubTeam.objects.filter(club_id=request.user.club_id, users=request.user)
-            request.seasons_list = ClubSeason.objects.filter(club_id=request.user.club_id)
-        else:
-            request.teams_list = UserTeam.objects.filter(user_id=request.user)
-            request.seasons_list = UserSeason.objects.filter(user_id=request.user)
+        if not request.user.is_anonymous:
+            if request.user.club_id is not None:
+                request.teams_list = ClubTeam.objects.filter(club_id=request.user.club_id, users=request.user)
+                request.seasons_list = ClubSeason.objects.filter(club_id=request.user.club_id)
+            else:
+                request.teams_list = UserTeam.objects.filter(user_id=request.user)
+                request.seasons_list = UserSeason.objects.filter(user_id=request.user)
 
         response = self.get_response(request)
 
