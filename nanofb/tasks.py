3f9e50acadb98478027488197492a5ce1f0971b3
DESKTOP-Q4NDK8R\Nickolay <test>
2022-11-04 11:04:19 +0300

    no message

--- nanofb/tasks.py
+++ nanofb/tasks.py
@@ -3,12 +3,14 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-from nanofb.shared.models import SharedLink
-from nanofb.shared.v_api import check_link_expiration
-
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'nanofb.settings'
 django.setup()
+
+import sys
+sys.path.insert(0,'..')
+from shared.models import SharedLink
+from shared.v_api import check_link_expiration
 
 app = Celery()
 @app.on_after_configure.connect
