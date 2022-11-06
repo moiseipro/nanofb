9a5fb31874cce3c3781d53b69270a8933431134d
DESKTOP-Q4NDK8R\Nickolay <test>
2022-11-06 10:23:30 +0300

    update celery

--- nanofb/celery.py
+++ nanofb/celery.py
@@ -7,7 +7,7 @@ from shared.models import SharedLink
 from shared.v_api import check_link_expiration
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nanofb.settings')
-app = Celery('nanofb')
+app = Celery('celery')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
