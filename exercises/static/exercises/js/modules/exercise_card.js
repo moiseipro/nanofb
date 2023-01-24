aa8d9c4603f3d74dda9591cf247d07d681f1014e
DESKTOP-Q4NDK8R\Nickolay <test>
2023-01-24 14:59:17 +0300

    update exs card and list

--- exercises/static/exercises/js/modules/exercise_card.js
+++ exercises/static/exercises/js/modules/exercise_card.js
@@ -139,7 +139,7 @@ function LoadExerciseOne(exsID = null, fromNFB = 0, folderType = "") {
         },
         complete: function (res) {
             $('.page-loader-wrapper').fadeOut();
-            window.lastListUsed = "exercises";
+            window.canChangeExs = true;
         }
     });
     try {
@@ -1669,9 +1669,34 @@ $(function() {
 
     $('#exerciseCard').on('change', '.exs_edit_field', (e) => {
         window.changedData = true;
-        if (!$(e.currentTarget).prop('required')) {return;}
-        let isEmpty = $(e.currentTarget).val() == '';
-        $(e.currentTarget).toggleClass('empty-field', isEmpty);
+        if (!$(e.currentTarget).prop('required') && !$(e.currentTarget).hasClass('field_pair')) {return;}
+        if ($(e.currentTarget).prop('required')) {
+            let isEmpty = $(e.currentTarget).val() == '';
+            $(e.currentTarget).toggleClass('empty-field', isEmpty);
+        } else if ($(e.currentTarget).hasClass('field_pair')) {
+            let cName = $(e.currentTarget).attr('name');
+            if (cName == "field_age_a") {
+                let secondValElem = $('#exerciseCard').find('.exs_edit_field[name="field_age_b"]');
+                if (secondValElem.val() == "") {
+                    $(secondValElem).val($(e.currentTarget).val());
+                }
+            } else if (cName == "field_age_b") {
+                let secondValElem = $('#exerciseCard').find('.exs_edit_field[name="field_age_a"]');
+                if (secondValElem.val() == "") {
+                    $(secondValElem).val($(e.currentTarget).val());
+                }
+            } else if (cName == "field_players_a") {
+                let secondValElem = $('#exerciseCard').find('.exs_edit_field[name="field_players_b"]');
+                if (secondValElem.val() == "") {
+                    $(secondValElem).val($(e.currentTarget).val());
+                }
+            } else if (cName == "field_players_b") {
+                let secondValElem = $('#exerciseCard').find('.exs_edit_field[name="field_players_a"]');
+                if (secondValElem.val() == "") {
+                    $(secondValElem).val($(e.currentTarget).val());
+                }
+            }
+        }
     });
 
 
