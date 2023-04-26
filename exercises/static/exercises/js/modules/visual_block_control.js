ec49d280263b236027edd2b2bb57d9534a857069
DESKTOP-Q4NDK8R\Nickolay <test>
2023-04-26 16:44:45 +0300

    no message

--- exercises/static/exercises/js/modules/visual_block_control.js
+++ exercises/static/exercises/js/modules/visual_block_control.js
@@ -10,6 +10,11 @@ function RenderCarouselAll() {
     $(elems).find('video').attr('id', '');
     $('.visual-block').find('#carouselAll').find('.carousel-inner > div').removeClass('active');
     $('.visual-block').find('#carouselAll').find('.carousel-inner > div').first().addClass('active');
+
+    $('#carouselAll').find('.carousel-item').find('.img-lazyload').each((index, elem) => {
+        $(elem.currentTarget).removeClass('d-none');
+        $(elem.currentTarget).prev().addClass('d-none');
+    });
 }
 
 
