727d5ca9e375055f91fbf77e0a8e5cd3f18339ee
moiseipro <sasha.makeeff@gmail.com>
2023-04-27 14:05:13 +0300

    Fix share training

--- shared/static/shared/js/base_shared_training.js
+++ shared/static/shared/js/base_shared_training.js
@@ -1,8 +1,20 @@
 $(window).on('load', function () {
-    $('.calculate-name input').each(function( index ) {
+    $('.calculate-name').each(function( index ) {
         let name_obj = $(this)
         let names = $.parseJSON(name_obj.attr('data-value').replace(/\'/g, '"'));
-        name_obj.val(get_translation_name(names))
+        name_obj.find('.title').text(get_translation_name(names))
         console.log(get_translation_name(names))
     })
+
+    let items = $('.video-js');
+    for (let i = 0; i < items.length; i++) {
+        let tId = $(items[i]).attr('id');
+        videojs($(`#${tId}`)[0], {
+            preload: 'auto',
+            autoplay: false,
+            controls: true,
+            aspectRatio: '16:9',
+            youtube: { "iv_load_policy": 1, 'modestbranding': 1, 'rel': 0, 'showinfo': 0, 'controls': 0 },
+        });
+    }
 })
