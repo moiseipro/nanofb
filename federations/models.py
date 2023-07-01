30ebc5822fa9a38eb6d2e6fed3822299694dc425
moiseipro <sasha.makeeff@gmail.com>
2023-07-01 10:59:51 +0300

    Add base federation models

--- federations/models.py
+++ federations/models.py
@@ -1,42 +1,36 @@
-from datetime import date
-
 from django.db import models
+from datetime import date
+from version.models import FederationLimitations, Version
 from django.utils.translation import gettext_lazy as _
 from django.utils.translation import pgettext_lazy as _p
 
 from django.contrib.auth.models import Permission, Group
 
-from federations.models import Federation
-from version.models import ClubLimitations
-
 
 # Create your models here.
-class Club(ClubLimitations):
+class Federation(FederationLimitations):
     name = models.CharField(
         max_length=100,
         verbose_name=_('title'),
-        help_text=_('Club title. Maximum length of 100 characters.'),
+        help_text=_('Federation title. Maximum length of 100 characters.'),
     )
     subdomain = models.CharField(
         max_length=10,
         verbose_name=_('subdomain'),
         help_text=_('The domain that will be displayed. The maximum length is 10 characters.'),
     )
-    federation_id = models.ForeignKey(
-        Federation,
-        null=True,
+    versions = models.ManyToManyField(
+        Version,
         blank=True,
-        on_delete=models.SET_NULL,
-        default=None,
-        verbose_name=_('Federation'),
-        help_text=_('The federation the club is a member of'),
+        verbose_name=_('Versions'),
+        help_text=_('Versions available to the federation for issuance'),
     )
     groups = models.ManyToManyField(
         Group,
         verbose_name=_("groups"),
         blank=True,
         help_text=_(
-            "The groups this club belongs to. A club will get all permissions "
+            "The groups this federation belongs to. A club will get all permissions "
             "granted to each of their groups."
         ),
     )
@@ -44,12 +38,12 @@ class Club(ClubLimitations):
         Permission,
         verbose_name=_("permissions"),
         blank=True,
-        help_text=_("Specific permissions for this club."),
+        help_text=_("Specific permissions for this federation."),
     )
     date_registration = models.DateField(
         auto_now_add=True,
         verbose_name=_('date registration'),
-        help_text=_('Date of registration of the club in the program.'),
+        help_text=_('Date of registration of the federation in the program.'),
     )
     date_registration_to = models.DateField(
         default=date.today,
@@ -61,7 +55,7 @@ class Club(ClubLimitations):
     block = models.BooleanField(
         default=False,
         verbose_name=_('block'),
-        help_text=_('Is the club blocked?'),
+        help_text=_('Is the federation blocked?'),
         null=False,
         blank=False,
     )
@@ -70,11 +64,11 @@ class Club(ClubLimitations):
         return self.name
 
     class Meta:
-        verbose_name = _('Club card')
-        verbose_name_plural = _('Club cards')
+        verbose_name = _('Federation card')
+        verbose_name_plural = _('Federation cards')
         permissions = [
             (
-                "club_admin",
-                _('Access to the club admin panel')
+                "federation_admin",
+                _('Access to the federation admin panel')
             )
         ]
