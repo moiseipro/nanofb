a792e03168f3edb8266ce5bed93730bc5a4e8563
moiseipro <sasha.makeeff@gmail.com>
2023-11-28 12:54:56 +0300

    Add function delete all sent notifications

--- notifications/static/notifications/js/base_notification.js
+++ notifications/static/notifications/js/base_notification.js
@@ -185,11 +185,43 @@ $(window).on("load", function () {
     notification_table.on('click', '.delete-notification', function (){
         let id = $(this).attr('data-id')
         let send_data = {}
-
-        ajax_notification_action('DELETE', send_data, 'notification', id).then(function (data) {
-            console.log(data)
-            notification_table.ajax.reload();
+        swal({
+            title: gettext("Removal"),
+            text: gettext("Delete the notification template or delete all sent ones?"),
+            icon: "warning",
+            buttons: {
+                catch: {
+                    text: gettext("Delete a template"),
+                    value: "delete-template",
+                },
+                defeat: {
+                    text: gettext("Delete all sent"),
+                    value: "delete-sent",
+                },
+                cancel: gettext("Cancel"),
+            },
+            dangerMode: true,
         })
+        .then((value) => {
+            switch (value) {
+                case "delete-template":
+                    ajax_notification_action('DELETE', send_data, 'notification', id).then(function (data) {
+                        console.log(data)
+                        notification_table.ajax.reload();
+                        notification_sent_table.ajax.reload();
+                    })
+                    break;
+                case "delete-sent":
+                    ajax_notification_action('POST', send_data, 'delete notification', id, 'delete_notification').then(function (data) {
+                        console.log(data)
+                        notification_sent_table.ajax.reload();
+                    })
+                    break;
+                default:
+                    break;
+            }
+        });
+
     })
 
     notification_table.on('click', '.edit-notification', function (){
