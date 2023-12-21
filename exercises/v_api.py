830331068f1c15337525ecfeec2f127e578b231c
DESKTOP-Q4NDK8R\Nickolay <test>
2023-12-21 18:57:28 +0300

    update exs filter

--- exercises/v_api.py
+++ exercises/v_api.py
@@ -622,14 +622,14 @@ def get_excerises_data(folder_id=-1, folder_type="", req=None, cur_user=None, cu
                 Q(userexerciseparam__video_1_watched=True) |
                 Q(userexerciseparam__animation_1_watched=True)
             )
-        )
+        ).distinct()
     if filter_video_watched_not != -1:
         f_exercises = f_exercises.filter(
             Q(
                 Q(Q(userexerciseparam__video_1_watched=False) | Q(userexerciseparam__video_1_watched__isnull=True)) &
                 Q(Q(userexerciseparam__animation_1_watched=False) | Q(userexerciseparam__animation_1_watched__isnull=True))
             )
-        )
+        ).distinct()
     if filter_video_isvideo != -1:
         f_exercises = f_exercises.filter(
             Q(exercisevideo__video__note__video=True)
