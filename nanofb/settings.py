36953f4ede5d4df8ab49b3f6be13a1813616dce2
DESKTOP-MS1VD3N\Nickolay <shavnick.main@gmail.com>
2025-02-07 20:36:35 +0300

    add new database connection for kairat club

--- nanofb/settings.py
+++ nanofb/settings.py
@@ -160,14 +160,26 @@ DB_beget = {
         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
     },
 }
-
 DB_test = {
     'ENGINE': 'django.db.backends.sqlite3',
     'NAME': BASE_DIR / 'db.sqlite3',
 }
 
+DB_club__kairat = {
+   'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'nanofb',
+    'USER': 'nanofb',
+    'PASSWORD': 'AsVev9Jzcy6f',
+    'HOST': '45.88.90.61',
+    'PORT': '3306',
+    'OPTIONS': {
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+    },
+}
+
 DATABASES = {
-    'default': DB_localhost
+    'default': DB_localhost,
+    'club_db__kairat': DB_club__kairat
 }
 
 
