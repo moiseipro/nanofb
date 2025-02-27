7c8d87ec8bf7735966135bf3ffa302f552a7f648
DESKTOP-MS1VD3N\Nickolay <shavnick.main@gmail.com>
2025-02-27 11:07:16 +0300

    no message

--- authorization/forms.py
+++ authorization/forms.py
@@ -228,5 +228,5 @@ class NewUserForm(UserCreationForm):
 
 class NewLoginForm(AuthenticationForm):
     username = forms.EmailField(required=True, widget=text_widget)
-    # password = forms.CharField(required=True, widget=password_widget, label="Пароль")
+    password = forms.CharField(required=True, widget=password_widget, label="Пароль")
 
