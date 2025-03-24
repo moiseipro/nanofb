43515adb5443304801c05a77af68a7fbb6a9a984
DESKTOP-MS1VD3N\Nickolay <shavnick.main@gmail.com>
2025-03-24 21:28:50 +0300

    no message

--- nanofb/settings.py
+++ nanofb/settings.py
@@ -37,11 +37,14 @@ ALLOWED_HOSTS = [
     'https.nanofootball.ru', 'https://www.nanofootball.ru', 'https://nanofootball.ru', 'http://nanofootball.ru',
     'nanofootball.info', 'www.nanofootball.info', 'http.nanofootball.info',
     'https.nanofootball.info', 'https://www.nanofootball.info', 'https://nanofootball.info', 'http://nanofootball.info',
+    'nanofootball.kz', 'www.nanofootball.kz', 'http.nanofootball.kz',
+    'https.nanofootball.kz', 'https://www.nanofootball.kz', 'https://nanofootball.kz', 'http://nanofootball.kz',
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://nanofootball.com', 'https://www.nanofootball.com', 
     'https://nanofootball.ru', 'https://www.nanofootball.ru',
     'https://nanofootball.info', 'https://www.nanofootball.info',
+    'https://nanofootball.kz', 'https://www.nanofootball.kz',
 ]
 
 # Application definition
