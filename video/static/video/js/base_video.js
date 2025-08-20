6dcba13e8f6951d4b0f6b0cd4250b27d9b6eb11d
DESKTOP-MS1VD3N\Nickolay <shavnick.main@gmail.com>
2025-08-20 14:08:59 +0300

    no message

--- video/static/video/js/base_video.js
+++ video/static/video/js/base_video.js
@@ -12,6 +12,9 @@ function formatState (state) {
 $(window).on('load', function (){
     $('.video-source').select2({
         templateResult: formatState,
+        dropdownCss: {
+            'max-height': '10vh'
+        }
     })
     $('.exercise-folder').select2({
         templateResult: formatState,
