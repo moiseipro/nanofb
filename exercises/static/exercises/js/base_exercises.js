2ece66c9e476afc7ed9e64c747a3e000580795d0
DESKTOP-MS1VD3N\Nickolay <shavnick.main@gmail.com>
2025-11-04 22:27:47 +0300

    update @exercises

--- exercises/static/exercises/js/base_exercises.js
+++ exercises/static/exercises/js/base_exercises.js
@@ -3469,6 +3469,8 @@ $(function() {
         ToggleMarkersInExs();
     }
     $('#toggleExsEditPanel').on('click', (e) => {
+        swal("Внимание!", "Данная функция временно недоступна.", "info");
+        return;
         $('.exs-edit-block').toggleClass('d-none');
         $(e.currentTarget).toggleClass('selected3', !$('.exs-edit-block').hasClass('d-none'));
         // $('.btns-tabs-first').toggleClass('d-none', !$('.exs-edit-block').hasClass('d-none'));
